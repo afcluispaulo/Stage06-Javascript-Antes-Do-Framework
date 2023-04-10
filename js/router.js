0b09bd3cfb0368454aecbe214de71480e5d852ec
Luis Paulo <luispaulo5@hotmail.com>
2023-04-10 16:18:32 -0300

    adicionado função para mostrar a rota atual no console

--- js/router.js
+++ js/router.js
@@ -4,4 +4,13 @@ export class Router {
     add(routeName, page) {
         this.routes[routeName] = page
     }
+
+     route(event) {
+        event = event || window.event
+        event.preventDefault()
+
+        window.history.pushState({}, "", event.target.href)
+
+        this.handle
+     }
 }

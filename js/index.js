84c664051d7df945fdf8236b6763d5608f4faee2
Luis Paulo <luispaulo5@hotmail.com>
2023-04-10 23:31:16 -0300

    adicionado novos nomes
    mudado o nome das imagens, e da rota explorer

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import { Router } from './router.js'
 const router = new Router()
 router.add('/', "pages/home.html")
 router.add("/theuniverse", "/pages/theuniverse.html")
-router.add("/exploration", "/pages/exploration.html")
+router.add("/explorer", "/pages/explorer.html")
 router.add(404, "/pages/404.html")
 
 router.handle()

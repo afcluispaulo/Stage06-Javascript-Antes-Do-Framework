29f1945dad75c024dcfcb54f25c519c87eaf4487
Luis Paulo <luispaulo5@hotmail.com>
2023-04-14 10:15:39 -0300

    ajustado menu
    adicionado troca de classesw do menu

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
 import { Router } from './router.js'
+const btnHome = document.getElementById("home")
+const homeMenu = document.querySelector(".homeMenu")
+
+const btnTheUnverse = document.getElementById("theuniverse")
+const theUniverseMenu = document.querySelector(".theUniverseMenu")
+
+btnHome.addEventListener('click', function() {
+    homeMenu.classList.add("navClick")
+
+    theUniverseMenu.classList.remove("navClick")
+})
+
+btnTheUnverse.addEventListener('click', function() {
+    theUniverseMenu.classList.add("navClick")
+
+    homeMenu.classList.remove("navClick")
+
+})
+
+
 
 const router = new Router()
 router.add('/', "pages/home.html")

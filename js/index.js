ea3c5c017d3fd23a197a4c60958927ca4fe4099b
Luis Paulo <luispaulo5@hotmail.com>
2023-04-14 10:38:36 -0300

    ajustado menu
    adicionado classes no botao explorer

--- js/index.js
+++ js/index.js
@@ -5,21 +5,29 @@ const homeMenu = document.querySelector(".homeMenu")
 const btnTheUnverse = document.getElementById("theuniverse")
 const theUniverseMenu = document.querySelector(".theUniverseMenu")
 
+const btnTheExplorer = document.getElementById("theexplorer")
+const theExplorerMenu = document.querySelector(".theExplorerMenu")
+
 btnHome.addEventListener('click', function() {
     homeMenu.classList.add("navClick")
 
     theUniverseMenu.classList.remove("navClick")
+    theExplorerMenu.classList.remove("navClick")
 })
 
 btnTheUnverse.addEventListener('click', function() {
     theUniverseMenu.classList.add("navClick")
 
     homeMenu.classList.remove("navClick")
-
+    theExplorerMenu.classList.remove("navClick")
 })
 
+btnTheExplorer.addEventListener('click', function() {
+    theExplorerMenu.classList.add("navClick")
 
-
+    homeMenu.classList.remove("navClick")
+    theUniverseMenu.classList.remove("navClick")
+})
 const router = new Router()
 router.add('/', "pages/home.html")
 router.add("/theuniverse", "/pages/theuniverse.html")

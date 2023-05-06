1f2a5048126ddbdf839dfee5c63a6ed407bd74db
Luis Paulo <luispaulo5@hotmail.com>
2023-05-06 00:48:24 -0300

    adicionado jvascript  main, e favorites

---
+++ Github Favorites/js/Favorites.js
@@ -0,0 +1,13 @@
+export class Favorites {
+    constructor(root) {
+        this.root = document.querySelector(root)
+
+    }
+}
+
+export class FavoritesView extends Favorites {
+    constructor(root) {
+        super(root)
+        this.tbody = this.root.querySelector('table tbody')
+    }
+}

d4498e7aac18c00d00389e36107def359f82735f
Luis Paulo <luispaulo5@hotmail.com>
2023-05-08 15:09:39 -0300

    adicionado função createRow()
    adicionado função responsável pela conectividade com o html do formulário

--- Github Favorites/js/Favorites.js
+++ Github Favorites/js/Favorites.js
@@ -11,5 +11,31 @@ export class FavoritesView extends Favorites {
         this.tbody = this.root.querySelector('table tbody')
     }
 
-    
+    createRow() { 
+
+        const tr = document.createElement('tr')
+
+        tr.innerHTML = `
+        <tr>
+            <td class="user">
+                <img src="https://github.com/maykbrito.png" alt="Imagem de maykbrito">
+                <a href="https://github.com/maykbrito" target="_blank">
+                <p>Mayk Brito</p>
+                <span>maykbrito</span>
+                </a>
+            </td>
+            <td class="repositories">
+                76
+            </td>
+            <td class="followers">
+            9589  
+            </td>
+            <td>
+                <button class="remove">&times;</button>
+            </td>
+        </tr>
+        `
+
+        return tr
+    }
 }
